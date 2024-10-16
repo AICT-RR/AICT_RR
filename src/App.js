ccba3d5f92b7fe2ad97ce9c8a176470314418596
seoftbh <48661442+seoftbh@users.noreply.github.com>
2024-10-16 16:05:56 +0900

    리액트 라우터 구성

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import SlideScreen from './pages/SlideScreen';
 import Settings from './pages/Settings';
+import DisplaySetting from './pages/DisplaySetting';
+import SlideSetting from './pages/SlideSetting';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={ <SlideScreen /> } />
-        <Route path="/settings" element={ <Settings /> } />
+        <Route path="/settings" element={ <Settings /> }>
+          <Route path="display" element={ <DisplaySetting /> } />
+          <Route path="slide" element={ <SlideSetting /> } />
+        </Route>
       </Routes>
     </div>
   );
