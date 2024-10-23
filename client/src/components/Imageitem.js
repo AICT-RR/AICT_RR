3ee4cc4a6411c69ef369179c185aa46b47509687
seoftbh <48661442+seoftbh@users.noreply.github.com>
2024-10-23 23:15:33 +0900

    file_upload UI 결합 완료

--- client/src/components/Imageitem.js
+++ client/src/components/Imageitem.js
@@ -1,47 +1,48 @@
 import React from 'react';
-import styled from "styled-components";
-import { useSortable } from "@dnd-kit/sortable";
+import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 
-const Container = styled.div`
-  width: 150px;
-  height: 150px;
-  background-color: white;
-  border-radius: 4px;
-  border: 1px solid gray;
-  text-align: center;
-  margin: 10px;
-`;
+function ImageItem({ fileName, onDelete }) {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: fileName });
 
-const ImageItem = ({ fileName, onDelete }) => {
-    const {
-        attributes,
-        listeners,
-        setNodeRef,
-        transform,
+    const style = {
+        transform: CSS.Transform.toString(transform),
         transition,
-        isDragging,
-    } = useSortable({ id: fileName });
+        padding: '10px',
+        margin: '10px',
+        backgroundColor: '#fff',
+        border: '1px solid #ccc',
+        borderRadius: '5px',
+        position: 'relative',
+    };
 
     return (
-        <Container
-            ref={setNodeRef}
-            {...attributes}
-            {...listeners}
-            style={{
-                transform: CSS.Transform.toString(transform),
-                transition,
-                zIndex: isDragging ? '100' : undefined,
-            }}
-        >
+        <div ref={setNodeRef} style={style} {...attributes}>
             <img
                 src={`/uploads/${fileName}`}
                 alt={fileName}
-                style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
+                style={{ width: '150px', height: '150px', objectFit: 'cover', cursor: 'grab' }} // 커서 스타일 변경
+                {...listeners} // 드래그 리스너를 이미지에만 적용
             />
-            <button onClick={() => onDelete(fileName)}>X</button>
-        </Container>
+            <button
+                onClick={() => onDelete(fileName)}
+                style={{
+                    position: 'absolute',
+                    top: '5px',
+                    right: '5px',
+                    background: 'red',
+                    color: '#fff',
+                    border: 'none',
+                    borderRadius: '50%',
+                    width: '20px',
+                    height: '20px',
+                    cursor: 'pointer',
+                }}
+            >
+                X
+            </button>
+        </div>
     );
-};
+}
 
 export default ImageItem;
