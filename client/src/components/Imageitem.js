578f24e22a66ae6a92c653c26105ee8c2ca883bb
kimguppy <wonny2333@naver.com>
2024-10-26 17:55:41 +0900

    start

--- client/src/components/Imageitem.js
+++ client/src/components/Imageitem.js
@@ -25,24 +25,24 @@ function ImageItem({ fileName, onDelete }) {
                 style={{ width: 'auto', height: '150px', objectFit: 'cover', cursor: 'grab' }} // 커서 스타일 변경
                 {...listeners} // 드래그 리스너를 이미지에만 적용
             />
-<button
-    onClick={() => onDelete(fileName)}
-    style={{
-        position: 'absolute',
-        top: '5px',
-        right: '5px',
-        background: 'red',
-        color: '#fff',
-        border: 'none',
-        borderRadius: '50%',
-        width: '30px',  
-        height: '30px',
-        cursor: 'pointer',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    }}
->
+            <button
+                onClick={() => onDelete(fileName)}
+                style={{
+                    position: 'absolute',
+                    top: '5px',
+                    right: '5px',
+                    background: 'red',
+                    color: '#fff',
+                    border: 'none',
+                    borderRadius: '50%',
+                    width: '30px',  
+                    height: '30px',
+                    cursor: 'pointer',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
     <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
         <path d="M6 18L18 6M6 6l12 12" stroke="#fff" strokeWidth="5" />
     </svg>
