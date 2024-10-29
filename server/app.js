b31086aea29983c887f173a1de2d849303f08c18
seoftbh <48661442+seoftbh@users.noreply.github.com>
2024-10-29 20:39:15 +0900

    fix listen port

--- server/app.js
+++ server/app.js
@@ -289,6 +289,9 @@ app.post('/update-file-order', (req, res) => {
 
 
 // 서버 시작
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
